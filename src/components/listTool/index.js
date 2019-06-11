476c25ef1f2f0b9a8fdf9f7d12a946c43d3ed5bd
Julio Melo <julio@alfamultimidia.com.br>
2019-06-10 21:33:16 -0300

    Componente de listagem de tools modificado para statefull component

--- src/components/listTool/index.js
+++ src/components/listTool/index.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Tool } from './styles';
 import RemoveImg from '../../assets/images/Icon-Delete-2px.svg';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as ToolsActions from '../../store/actions/tools';
 
-const ListTool = ( {state, removeTool} ) => (
-  <Container>
-      {
-        state.tools.map( tool => 
-          <Tool>
-            <div className="title">
-              <h1> { tool.title } </h1>
-              <img  src={ RemoveImg } onClick={() => removeTool(tool.id)} alt="button remove"/>
-            </div>
+class ListTool extends Component {
+  
+  constructor(props) {
+    super(props);
+    
+  }
 
-            <div className="description">
-              <p>{ tool.description }</p>
-            </div>
-            
-            <div className="tags">
-              {  tool.tags.map( tag => 
-                  <span>#{ tag }</span>
-                )
-              }
-            </div> 
-          </Tool>
-        )
-      }
-  </Container>
-);
+  render() {
+    return(
+      <Container>
+          {
+            this.props.state.tools.map( tool => 
+              <Tool key={tool.id}>
+                <div className="title">
+                  <h1> { tool.title } </h1>
+                  <img  src={ RemoveImg } onClick={() => this.props.removeTool(tool.id)} alt="button remove"/>
+                </div>
+
+                <div className="description">
+                  <p>{ tool.description }</p>
+                </div>
+                
+                <div className="tags">
+                  {  tool.tags.map( tag => 
+                      <span>#{ tag }</span>
+                    )
+                  }
+                </div> 
+              </Tool>
+            )
+          }
+      </Container>
+    );
+  }
+};
 
 const mapStateToProps = state => ({
   state,
